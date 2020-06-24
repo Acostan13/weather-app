9ed8c65dffac2f966bc44f5b51e1d2ac57184c31
Acostan13 <acostan@fau.edu>
2020-06-24 15:55:39 -0400

    Updates

---
+++ precache-manifest.719e83ab82f0fea6b82f4412018bd8f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6acf28efe63aa2f7edf2f91514f3ae6d",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "3fbb09e094e4e2038417",
+    "url": "/weather-app/static/css/main.e8dbbeda.chunk.css"
+  },
+  {
+    "revision": "6f28c41d498abbd90061",
+    "url": "/weather-app/static/js/2.d0f5f71e.chunk.js"
+  },
+  {
+    "revision": "85b12af60ddf57ea6860c3b16407fd52",
+    "url": "/weather-app/static/js/2.d0f5f71e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3fbb09e094e4e2038417",
+    "url": "/weather-app/static/js/main.fecba040.chunk.js"
+  },
+  {
+    "revision": "bcd9f72acb16be0c9209",
+    "url": "/weather-app/static/js/runtime-main.e07bc655.js"
+  }
+]);
